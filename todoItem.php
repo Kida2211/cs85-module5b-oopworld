18509fab74c9bdb88a4cab5be3b090dfa0f6a554
Nathan Kida <nathankida@gmail.com>
2025-07-11 21:35:34 -0700

    Add summary, daysLeft, and markComplete methods

--- todoItem.php
+++ todoItem.php
@@ -13,5 +13,23 @@
             $this->priority = $priority;
             $this->iscompleted = $iscompleted;
         }
+
+        //summary display method
+        public function getSummary() {
+           return "Task: $this->title\nDescription: $this->description\nDue: $this->dueDate\nCompleted: " . ($this->iscompleted ? "Yes" : "No") . "\nPriority: $this->priority\n";
+        }
+
+        // Return calculated value (days left)
+        public function daysLeft() {
+            $today = date_create(date("Y-m-d"));
+            $due = date_create($this->dueDate);
+            $interval = date_diff($today, $due);
+            return $interval->format("%a");
+        }
+
+        // Change property method
+        public function markComplete() {
+            $this->iscompleted = true;
+        }
     }
 ?>
